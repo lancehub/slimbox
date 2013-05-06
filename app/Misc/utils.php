957e86fca375970f544ff0cd246e476fd356de49
william <arenabpo@gmail.com>
2013-05-06 07:58:57 -0700

    add get_config function etc

--- app/Misc/utils.php
+++ app/Misc/utils.php
@@ -1,3 +1,39 @@
 <?php
 
 //all util functions
+
+function debug($o){
+	echo '<pre>';
+	print_r($o);
+	echo '</pre>';
+}
+
+function get_app(){
+	return \Slim\Slim::getInstance();
+}
+
+function get_config($key = ''){
+	global $config;
+	if(!empty($key)){
+		if(isset($config[$key])){
+			return $config[$key];
+		}else{
+			if(strstr($key,'.')!== false){
+				$parts = explode('.',$key);
+				$config_value = $config;
+				foreach($parts as $part){
+					if(isset($config_value[$part])){
+						$config_value = $config_value[$part];
+					}else{
+						return null;
+					}
+				}
+				return $config_value;
+			}else{
+				return null;
+			}
+		}
+	}else{
+		return $config;
+	}
+}
